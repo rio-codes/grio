9a25f1a1e148e807d281e76d8f42232ef9235a7f
rio-codes <rio_dev@mailbox.org>
2022-08-06 09:55:00 -0700

    config file function works

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import datetime
 import argparse
 import os
+import sys
 import configparser
 
-from scp import SCPClient
+# from scp import SCPClient
 from paramiko import SSHClient
 
 def config():
@@ -22,48 +23,66 @@ def config():
 def cli():
 
     parser = argparse.ArgumentParser(description='Post short updates to a microblog on a remote server')
-    subparsers = parser.add_subparsers(help = 'post or configure grio')
+    subparsers = parser.add_subparsers(help = 'post or configure grio', dest='command')
 
     postParser = subparsers.add_parser('post')
     configParser = subparsers.add_parser('config')
 
-    postParser.add_argument('post', metavar = 'post', help = 'the content of the post', required=False)
-
-    configParser.add_argument('config', metavar = 'config', help = 'set up configuration file', default=False, required=False)
-    configParser.add_argument('-s', '--server', metavar = 'server', help = 'the server to upload the post to', required=False)
-    configParser.add_argument('-p', '--port', metavar = 'port', help = 'the ssh port on the web server', required=False)
+#    postParser.add_argument('post', help = 'post to grio')
+    postParser.add_argument('post', action = 'store', nargs='?', help = 'the content of the post')
+    configParser.add_argument('config', help = 'set up configuration file')
+    configParser.add_argument('-s', '--server', help = 'the server to upload the post to', required=True)
+    configParser.add_argument('-p', '--port', help = 'the ssh port on the web server', required=True)
 
     args = parser.parse_args()
 
     return args
 
-def main():
-    timestamp = datetime.now()
+def post(content):
+    timestamp = str(datetime.datetime.now())
+    with open('grioblog.txt', 'a+') as f:
+        lines = f.readlines()
+        print("lines at top:")
+        print(lines)
+#        f.close()
+#    with open('grioblog.txt', 'a+') as f:
+        if not lines:
+            print("writing temp line")
+            f.write("This is the first line")
+        f.close()
+    with open('grioblog.txt', 'a+') as f:
+        lines = f.readlines()
+        print("next lines:")
+        print(lines)
+        f.write(timestamp + ' || ' + content + '\n')
+        for line in reversed(lines):
+            print("checking line")
+            f.write(line)
+#    n.close()
+        f.close()
+ #   os.rename('grioblog.txt', 'grioblog_old.txt')
+ #   os.rename('grioblog_new.txt', 'grioblog.txt')
 
-    if os.path.exists('grio.conf'):
-        config = configparser.ConfigParser()
-        config.read('grio.conf')
-        server = config['SSHSettings']['server']
-        port = config['SSHSettings']['server']
-    else:
-        print('No config file found, run "grio config" to setup')
-        exit
+def main():
+    config = configparser.ConfigParser()
 
     args = cli()
-    newPost = args.post
 
-#    if args.server:
-#        server = args.server
-#    else:
-
-    with open('grioblog.txt', 'r') as f:
-        with open('grioblog_new.txt', 'w') as n:
-            lines = f.readlines()
-            n.write(timestamp + newPost)
-            for line in lines:
-                n.write(line)
-            n.close()
-        f.close()
+    if args.command == 'post':
+        if os.path.exists('grio.conf'):
+            config.read('grio.conf')
+            server = config['SSHSettings']['server']
+            port = config['SSHSettings']['server']
+            post(args.post)
+        else:
+            print('No config file found, run "grio config" to setup')
+            sys.exit()
+    elif args.command == 'config':
+        config['SSHSettings'] = {}
+        config['SSHSettings']['server'] = args.server
+        config['SSHSettings']['port'] = args.port
+        with open('grio.conf', 'w') as configFile:
+            config.write(configFile)
 
 #ssh = SSHClient()
     #ssh.load_system_host_keys()
