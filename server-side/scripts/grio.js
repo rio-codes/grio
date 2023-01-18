9bbece2128480df1bf17756add2826ec6921beb3
rio-codes <rio_dev@mailbox.org>
2023-01-17 23:03:25 -0800

    fix: ignore commas in post content

--- server-side/scripts/grio.js
+++ server-side/scripts/grio.js
@@ -1,6 +1,6 @@
 $.get('grioblog.csv', function(data) {
     var posts = data.trim().split('\n');
-    var lastPost = (posts[posts.length - 1].split(','));
+    var lastPost = (posts[posts.length - 1].split(/,(.*)/));
     var timeNow = jQuery.timeago(new Date()); 
     var timeSince= jQuery.timeago(lastPost[0]); 
     $('.grio__timestamp').append(timeSince + '<br>');
